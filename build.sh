08290047dc4bd0788e87960c55cc9a0f22000654
Bengt Brodersen <bengt.brodersen@gmail.com>
2021-01-20 09:29:35 +0100

    feat: dark style

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 nativefier 'https://web.threema.ch' \
+  --background-color '#11181d' \
   --icon 'icon.png' --counter --bounce \
   --min-width 440 --min-height 484 \
   --darwin-dark-mode-support \
   --zoom 0.9 \
-  --inject 'app.css' \
+  --inject 'style.css' \
   --single-instance \
   --electron-version 11.1.1
   
