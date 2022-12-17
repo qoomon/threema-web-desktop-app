e8ffbfb53e2d0aa3cdec1cafdd403d2fab84d108
Bengt Brodersen <bengt.brodersen@gmail.com>
2022-12-17 22:27:16 +0100

    refactor

--- build.sh
+++ build.sh
@@ -4,10 +4,7 @@ nativefier 'https://web.threema.ch' \
   --background-color '#11181d' \
   --icon 'icon.png' --counter --bounce \
   --min-width 440 --min-height 484 \
+  --title-bar-style 'hiddenInset' \
+  --inject 'inject.js' \
   --darwin-dark-mode-support \
-  --zoom 0.9 \
-  --inject 'style.css' \
   --single-instance \
-  --electron-version 11.1.1
-  
-  # INFO electron version 11.2.0 is broken
